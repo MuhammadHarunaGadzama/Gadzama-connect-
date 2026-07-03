d7cf4e6106d244bc5cef47875d93bac62d18aaf4
MuhammadHarunaGadzama <muhammadhg2048@gmail.com>
2026-07-03 23:59:41 +0100

    app/login/page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../../lib/supabase";
+import { useRouter } from "next/navigation";
+
 export default function LoginPage() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    // if login successful → go to dashboard
+    router.push("/dashboard");
+  }
+
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#f8fafc",
-      }}
-    >
-      <div
-        style={{
-          background: "white",
-          padding: "40px",
-          borderRadius: "12px",
-          width: "350px",
-          boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
-        }}
-      >
+    <main style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "#f8fafc"
+    }}>
+      <div style={{
+        background: "white",
+        padding: "40px",
+        borderRadius: "12px",
+        width: "380px",
+        boxShadow: "0 2px 10px rgba(0,0,0,0.1)"
+      }}>
+        
         <h1 style={{ textAlign: "center", color: "#16a34a" }}>
           Login
         </h1>
@@ -25,24 +48,29 @@ export default function LoginPage() {
         <input
           type="email"
           placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           style={{
             width: "100%",
             padding: "12px",
-            marginTop: "20px",
+            marginTop: "20px"
           }}
         />
 
         <input
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           style={{
             width: "100%",
             padding: "12px",
-            marginTop: "15px",
+            marginTop: "15px"
           }}
         />
 
         <button
+          onClick={handleLogin}
           style={{
             width: "100%",
             padding: "12px",
@@ -50,11 +78,12 @@ export default function LoginPage() {
             background: "#16a34a",
             color: "white",
             border: "none",
-            borderRadius: "8px",
+            borderRadius: "8px"
           }}
         >
           Login
         </button>
+
       </div>
     </main>
   );
