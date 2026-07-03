642ae8a29a94b23b690dd5bafdfe3d18150166d7
MuhammadHarunaGadzama <muhammadhg2048@gmail.com>
2026-07-03 15:26:19 +0100

    components/Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#16a34a",
+        color: "white",
+        textAlign: "center",
+        padding: "20px",
+        marginTop: "40px",
+      }}
+    >
+      <p>© 2026 G-Connect. All rights reserved.</p>
+
+      <p style={{ marginTop: "10px" }}>
+        Connect. Learn. Grow.
+      </p>
+    </footer>
+  );
+}
